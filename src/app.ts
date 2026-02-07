f307d7348a1206b6ac6955d0f39f7e666563e654
Robinishear <mdrobinahmed57898@gmail.com>
2026-02-07 14:00:05 +0600

    ph healthcare management system server project start

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import { Application, Request, Response } from "express";
+import express from 'express';
+
+const app : Application  = express();
+
+
+
+// Enable URL-encoded form data parsing
+app.use(express.urlencoded({ extended: true }));
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// Basic route
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello, TypeScript + Express!');
+});
+
+export default app;
