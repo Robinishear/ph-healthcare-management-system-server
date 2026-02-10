147d599e12a4e4f0cdd6f777d53687143b9b90d0
Robinishear <mdrobinahmed57898@gmail.com>
2026-02-10 11:00:42 +0600

    Higher-Order Function (HOF)।,catchAsync Utility Function  Async Wrapper।

--- src/app/Module/specialty/specialty.controller.ts
+++ src/app/Module/specialty/specialty.controller.ts
@@ -1,92 +1,66 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { Request, Response } from "express";
+import { NextFunction, Request, RequestHandler, Response } from "express";
 import { SpecialtyService } from "./specialty.service";
 
-const createSpecialty = async (req: Request, res: Response) => {
-  try {
-    const Payload = req.body;
 
-    const Result = await SpecialtyService.createSpecialty(Payload);
-
-    res.status(201).json({
-      success: true,
-      message: "Hello Robin brother, you have truly succeeded.",
-      data: Result,
-    });
-  } catch (error: any) {
-    console.log(error);
-    res.status(500).json({
-      success: false,
-      message: "An error occurred while creating the specialty.",
-      error: error.message,
-    });
-  }
-};
-
-const getAllSpecialties = async (req: Request, res: Response) => {
-  try {
-    const specialty = await SpecialtyService.getAllSpecialties();
-    res.status(200).json({
-      success: true,
-      message: "All specialties retrieved successfully.",
-      data: specialty,
-    });
-  } catch (error: any) {
-    console.log(error);
-    res.status(500).json({
-      success: false,
-      message: "An error occurred while retrieving all specialties.",
-      error: error.message,
-    });
-  }
-};
-
-const deleteSpecialty = async (req: Request, res: Response) => {
-  try {
-    const { id } = req.params;
-    const Result = await SpecialtyService.deleteSpecialty(id as string);
-    res.status(200).json({
-      success: true,
-      message: "Specialty deleted successfully.",
-      data: Result,
-    });
-  } catch (error: any) {
-    console.log(error);
-    res.status(500).json({
-      success: false,
-      message: "An error occurred while deleting the specialty.",
-      error: error.message,
-    });
-  }
-};
-
-const updateSpecialty = async (req: Request, res: Response) => {
-  try {
-    const { id } = req.params;
-
-    if (!id) {
-      return res
-        .status(400)
-        .json({ success: false, message: "ID is required" });
+//* Higher-Order Function (HOF)।,catchAsync Utility Function  Async Wrapper।
+const catchAsync = (fn: RequestHandler) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      await fn(req, res, next);
+    } catch (error: any) {
+   
+      console.log(error);
+      res.status(500).json({
+        success: false,
+        message: "Internal Server Error",
+        error: error.message,
+      });
     }
-
-    const payload = req.body;
-    const result = await SpecialtyService.updateSpecialty(id as string, payload);
-
-    res.status(200).json({
-      success: true,
-      message: "Specialty updated successfully.",
-      data: result,
-    });
-  } catch (error: any) {
-    console.log(error);
-    res.status(500).json({
-      success: false,
-      message: "An error occurred while updating the specialty.",
-      error: error.message,
-    });
-  }
+  }; 
 };
+
+const createSpecialty = catchAsync(async (req: Request, res: Response) => {
+  const Payload = req.body;
+  const Result = await SpecialtyService.createSpecialty(Payload);
+
+  res.status(201).json({
+    success: true,
+    message: "Hello Robin brother, you have truly succeeded.",
+    data: Result,
+  });
+});
+
+const getAllSpecialties = catchAsync(async (req: Request, res: Response) => {
+  const Result = await SpecialtyService.getAllSpecialties();
+  res.status(200).json({
+    success: true,
+    message: "Specialties retrieved successfully.",
+    data: Result,
+  });
+});
+
+const deleteSpecialty = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const Result = await SpecialtyService.deleteSpecialty(id as string);
+  res.status(200).json({
+    success: true,
+    message: "Specialty deleted successfully.",
+    data: Result,
+  });
+});
+
+const updateSpecialty = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const Payload = req.body;
+  const Result = await SpecialtyService.updateSpecialty(id as string, Payload);
+
+  res.status(200).json({
+    success: true,
+    message: "Specialty updated successfully.",
+    data: Result,
+  });
+});
 
 export const SpecialtyController = {
   createSpecialty,
@@ -94,3 +68,4 @@ export const SpecialtyController = {
   deleteSpecialty,
   updateSpecialty,
 };
+
