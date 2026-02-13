ed96db3192e7b59f70905f1de88e2ea79e3c9601
Robinishear <mdrobinahmed57898@gmail.com>
2026-02-14 03:00:10 +0600

    user doctor

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { SpecialtyRoute } from "../module/specialty/specialty.route";
 import { AuthRoutes } from "../module/auth/auth.route";
+import { UserRoute } from "../module/user/user.route";
 
 
 const router = Router();
 router.use("/auth", AuthRoutes);
 router.use("/specialty", SpecialtyRoute);
+router.use("/user", UserRoute);
 
 export const IndexRoute = router;
